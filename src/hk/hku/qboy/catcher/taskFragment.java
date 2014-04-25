80188ecf7786610437c722b7f10e77c7ca1cb640
Queenie <cataroozhang@gmail.com>
2014-04-26 02:53:40 +0800

    error on startclick.

---
+++ src/hk/hku/qboy/catcher/taskFragment.java
@@ -0,0 +1,60 @@
+package hk.hku.qboy.catcher;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.ListFragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class taskFragment extends ListFragment {
+	static private final Uri tasks_provider = TaskProvider.CONTENT_URI;
+	//Context context = getActivity();
+	View V;
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        V = inflater.inflate(R.layout.main1, container, false);
+        setListAdapter();
+        addTaskButtonListener();
+        
+        return V;
+	}
+	
+	private void addTaskButtonListener() {
+		final Button add_button = (Button) V.findViewById(R.id.button1);
+		add_button.setBackgroundResource(R.drawable.plus);
+		// Add task button
+		View.OnClickListener add_button_on_click_listener = new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(getActivity(), TaskDetail.class);
+				startActivity(intent);
+			}
+		};
+		add_button.setOnClickListener(add_button_on_click_listener);
+	}
+
+	private void setListAdapter() {
+		//Cursor c = context.getContentResolver().query(uri, projection, selection, null, sortOrder);
+		Cursor cursor = getActivity().getContentResolver().query(tasks_provider, null, null, null, null);
+		TaskCursorAdapter taskAdapter = new TaskCursorAdapter(getActivity(), cursor);
+		this.setListAdapter(taskAdapter);
+	}
+	
+	public void taskStart(String currentTitle) {
+		Intent timerIntent = new Intent(getActivity(), TimerActivity.class);
+		timerIntent.putExtra("title", currentTitle);
+		startActivity(timerIntent);
+	}
+}
