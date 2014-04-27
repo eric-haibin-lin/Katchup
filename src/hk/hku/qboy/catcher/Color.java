255ba71ed8d8ceab022eb00eac03c11f48e86860
Eric Haibin Lin <linhaibin.eric@gmail.com>
2014-04-27 12:12:44 +0800

    Upload icon images.

---
+++ src/hk/hku/qboy/catcher/Color.java
@@ -0,0 +1,15 @@
+package hk.hku.qboy.catcher;
+
+public class Color {
+
+	public static String RED = "#f26c4f";
+	public static String GREY = "#625D5D";
+	public static String BLUE = "#A0CFEC";
+	public static String GREEN = "#64E986";
+	public static String YELLOW = "#FFE87C";
+	public static String PINK = "#FC6C85";
+
+	public Color() {
+	}
+
+}
